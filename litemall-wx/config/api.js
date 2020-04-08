15f352c4b875dbcc954fb1697a2546576e43270a
liutao <liutao12@renmaitech.com>
2020-04-08 23:00:16 +0800

    修改配置

--- litemall-wx/config/api.js
+++ litemall-wx/config/api.js
@@ -1,12 +1,12 @@
 // 以下是业务服务器API地址
 // 本机开发时使用
- var WxApiRoot = 'http://172.17.9.10:8080/wx/';
+// var WxApiRoot = 'http://172.17.9.10:8080/wx/';
 // 局域网测试使用
 // var WxApiRoot = 'http://192.168.1.3:8080/wx/';
 // 云平台部署时使用
 // var WxApiRoot = 'http://122.51.199.160:8080/wx/';
 // 云平台上线时使用
-// var WxApiRoot = 'https://www.menethil.com.cn/wx/';
+ var WxApiRoot = 'https://zwsee.com/litemall-admin-api/wx/';
 
 module.exports = {
   IndexUrl: WxApiRoot + 'home/index', //首页数据接口
